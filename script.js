aa6dc972be7acc093ac0d270b384486e6355ec01
tvoisyabr <neperamojny@gmail.com>
2024-06-27 10:43:12 -0700

    Add getCumputerChoice function to script.js. Computer will randomly pick 1 of 3 choices

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, World!")
+// Create a function that doesn't accept any parameters
+// Create an array that stores 3 values (rock, paper, scissors)
+// Make computer randomly chose one of the values form the array
+// Store the value into variable as a result
+// Make the function return the value
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    let randomChoice = choices[Math.floor(Math.random() * choices.length)];
+    return randomChoice;
+};
