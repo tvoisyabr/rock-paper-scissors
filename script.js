a11e450934a6e129d0cd058ed2826d841b4ca3ea
tvoisyabr <neperamojny@gmail.com>
2024-06-27 11:58:56 -0700

    Add getHumanChoice function to script.js. The function ask  user for a input and return it

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function getComputerChoice() {
     let randomChoice = choices[Math.floor(Math.random() * choices.length)];
     return randomChoice;
 };
+
+// Create a function that doesn't accept any parameters
+// Make the function accept input form a user
+// Make sure user prompts rock, paper or scissors (ignoring case sensitivity)
+// Store input form the user to a variable
+// Return user's input
+
+function getHumanChoice() {
+
+    let humanChoice;
+    let flag = false;
+    while(!flag) {
+        humanChoice = prompt("Rock, Paper, Scissors. What's your choice?");
+        humanChoice = humanChoice.toLowerCase();
+
+        if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
+            flag = true;
+            break;
+        }
+    }
+    return humanChoice;
+};
+
